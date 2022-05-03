53f1dacc504ed66e828b07594437c9d76f0048b9
Heran Mekonnen <mekonnen.heran@gmail.com>
2022-05-02 22:50:20 -0400

    Final Push- pokemon route file includes 3 .get versions (Note: 2/3 .gets are commented out) 1. using name, yields both pokemon name and url. 2. using id- due to items not having an i.d yields 'no results found' 3. using name-yields result of pokemon name, URL not displayed.

--- routes/api/pokemon.js
+++ routes/api/pokemon.js
@@ -3,10 +3,70 @@ const router = express.Router()
 const pokemon = require("../../Pokemon")
 
 
-//test run
-router.get('/pokemon',(req,res)=>{
-    res.status(200).json({pokemon});
+//View all pokemon in Database
+router
+.route("/pokemon")
+.get((req,res)=>{
+    res.status(200).json({pokemon:pokemon});
 })
+
+
+// Scroll down for additional / commented out ".gets"
+
+/**================================================================================================================= */
+
+        // Version 1: Task from assignment using name instead of ID - yielding display of both name and url 
+router
+.route("/pokemon/:name")
+.get((req, res)=>{
+    const found = pokemon.some(pokemon => pokemon.name === (req.params.name))
+
+    if (found){
+        res.status(200).json(pokemon.filter(pokemon =>pokemon.name === (req.params.name)));
+        ;
+    } else {
+        res.status(400).json({msg: `No pokemon by the name of ${req.params.name} found. Please check the name and try again.`})
+    }
+    
+})
+
+/**================================================================================================================= */
+/*
+Version 2: Assignment says "id" but none of the objects have id's so it will display content in the else condition
+
+router
+.route("/pokemon/:id")
+.get((req, res)=>{
+    const found = pokemon.some(pokemon => pokemon.id === (req.params.id))
+
+    if (found){
+        res.status(200).json({id: req.params.id});
+    } else {
+        res.status(400).json({msg: `No pokemon by the id of ${req.params.id} found. Please check the id and try again.`})
+    }
+    
+})
+*/
+
+
+/**================================================================================================================= */
+/*
+Version 3: Searches by name and returns pokemon by name is found in "database"
+
+router
+.route("/pokemon/:name")
+.get((req, res)=>{
+    const found = pokemon.some(pokemon => pokemon.name === (req.params.name))
+
+    if (found){
+        res.status(200).json({name: req.params.name});
+    } else {
+        res.status(400).json({msg: `No pokemon by the name of ${req.params.name} found. Please check the name and try again.`})
+    }
+    
+})
+*/
+
 
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// Import express
 const express = require("express")
 
-
+// Import router file
 const pokemon = require('./routes/api/pokemon')
 
 //Config
@@ -12,12 +13,13 @@ const server = express()
 // Body Parser MiddleWare
 server.use(express.json())
 
-// M.W to allow handling of form submissions
-server.use(express.urlencoded({extended:false}))
+// M.W to allow handling of form submissions- 
+// - not necessary at this time- server.use(express.urlencoded({extended:false}))
 
 // Connect server.js w/ route file 
 server.use('/', pokemon);
 
+// Homepage "welcome"
 server.get('/',(req,res)=>{
     res.status(200).json({message: 'Welcome to the Pokemon App!'});
 })
